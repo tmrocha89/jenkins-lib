679643d386b42dd988f0058f5c260ad9ac65d50d
Tiago Rocha <tiagorocha@takethewind.com>
2019-03-04 01:34:35 +0000

    fiz size

--- src/win/thebatman/tmrjenkinslib/compression/ZipCompressionBuilder.groovy
+++ src/win/thebatman/tmrjenkinslib/compression/ZipCompressionBuilder.groovy
@@ -45,7 +45,7 @@ class ZipCompressionBuilder implements ICompressionBuilder{
 
     private String getGlobParams(){
         StringBuilder strBuilder = new StringBuilder()
-        if (this.globPattern.length() > 0){
+        if (this.globPattern.size() > 0){
             this.globPattern.each{
                 strBuilder.append(it)
                 strBuilder.append(",")
